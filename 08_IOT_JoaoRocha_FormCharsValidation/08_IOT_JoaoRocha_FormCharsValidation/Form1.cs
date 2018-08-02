5757388302a90a4d9f6b484e19fc4677dff4d4e1
JoaoGSRocha <joaogsrocha@gmail.com>
2018-08-02 15:20:00 +0100

    [Add] Class 7 & 8 Content + Encapsulation of 7th class Methods
    - Class 8 uses Regex functionality;
    
    Notes: Class 7 has its methods ready for unit testing
    
    Keywords: Add, Class 7, Class 8, Encapsulation, Regex
    Importance: High

---
+++ 08_IOT_JoaoRocha_FormCharsValidation/08_IOT_JoaoRocha_FormCharsValidation/Form1.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace _08_IOT_JoaoRocha_FormCharsValidation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            textBox2.Enabled = HasText3ValidChars(textBox1);
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            textBox3.Enabled = HasText3ValidChars(textBox2);
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            richTextBox1.Enabled = HasText3ValidChars(textBox3);
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Determines whether TextBox.Text has 3 characterss, not including 
+        /// the whitespace
+        /// </summary>
+        /// <param name="textbox"></param>
+        /// <returns></returns>
+        private bool HasText3ValidChars(TextBox textbox)
+        {
+            string tmpText = Regex.Replace(textbox.Text, @"\s+", "");
+            if (tmpText.Length >= 3)
+                return true;
+            else
+                return false;
+        }
+
+        /// <summary>
+        /// Determines whether TextBox.Text has 3 characterss, not including 
+        /// the whitespace
+        /// </summary>
+        /// <param name="textbox"></param>
+        /// <returns></returns>
+        private bool HasText3ValidChars(RichTextBox textbox)
+        {
+            string tmpText = Regex.Replace(textbox.Text, @"\s+", "");
+            if (tmpText.Length >= 3)
+                return true;
+            else
+                return false;
+        }
+    }
+}
